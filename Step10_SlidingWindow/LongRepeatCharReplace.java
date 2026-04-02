e5fbd1f182ba9b6e8fded7fba697fef9a59e52c4
Abhishek Nobi <abhishek.nobi@gmail.com>
2026-04-02 14:48:34 +0530

    optimal done

--- Step10_SlidingWindow/LongRepeatCharReplace.java
+++ Step10_SlidingWindow/LongRepeatCharReplace.java
@@ -72,6 +72,26 @@ public class LongRepeatCharReplace {
         }
         return maxlen;
     }
+    static int optimal(String s,int k){
+        int l=0;
+        int r=0;
+        int maxlen=0;
+        int maxxfreq=0;
+
+        Map<Character,Integer> mpp =new HashMap<>();
+        while(r<s.length()){
+            mpp.put(s.charAt(r),mpp.getOrDefault(s.charAt(r),0)+1);
+            maxxfreq=Math.max(mpp.getOrDefault(s.charAt(r),0),maxxfreq);
+
+            if(r-l+1-maxlen>k){
+                mpp.put(s.charAt(l),(mpp.getOrDefault(s.charAt(l),0)-1));
+                l++;
+            }
+            maxlen=Math.max(r-l+1,maxlen);
+            r++;
+        }
+        return maxlen;
+    }
 
     static void main() {
        String s = "ABAB";
