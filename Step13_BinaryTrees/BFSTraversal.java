f495b3738d7231b7731910983dc690036c2e7edc
Abhishek Nobi <abhishek.nobi@gmail.com>
2026-04-16 16:44:15 +0530

    BFS done

---
+++ Step13_BinaryTrees/BFSTraversal.java
@@ -0,0 +1,53 @@
+package Step13_BinaryTrees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFSTraversal {
+    static void main() {
+        TreeNode root=new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(4);
+        root.right.right=new TreeNode(5);
+
+        List<List<Integer>> result=levelOrder(root);
+        System.out.print("Preorder Traversal: ");
+        // Output each value in the
+        // preorder traversal result
+        for ( List<Integer> val : result) {
+            System.out.println(val + " ");
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.data);
+
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
+}
