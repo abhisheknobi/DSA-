1f27453ea3ad029e606b74b1c6fd6b44fe6d17ee
Abhishek Nobi <abhishek.nobi@gmail.com>
2026-04-26 21:57:00 +0530

    array to treenode build

--- Step13_BinaryTrees/TreeNode.java
+++ Step13_BinaryTrees/TreeNode.java
@@ -1,5 +1,8 @@
 package Step13_BinaryTrees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     int data;
     TreeNode left;
@@ -9,5 +12,34 @@ public class TreeNode {
         data=val;
         left=null;
         right=null;
+    }
+    public static TreeNode buildTree(Integer[] nodes) {
+        if (nodes == null || nodes.length == 0 || nodes[0] == null) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode current = queue.poll();
+
+            // Process Left Child
+            if (i < nodes.length && nodes[i] != null) {
+                current.left = new TreeNode(nodes[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            // Process Right Child
+            if (i < nodes.length && nodes[i] != null) {
+                current.right = new TreeNode(nodes[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+        return root;
     }
 }
